abe4531c2fbeab19bdb540dd30d2dc4fcc4058fd
CteamToken <admin@christiantube.team>
2018-05-31 11:09:05 +0100

    Staking updates Version up.

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 8954;
+        nRPCPort = 38954;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 37546;
+        nRPCPort = 48954;
         strDataDir = "testnet4";
     }
 };
